d094f7fa81a6cbf39d0bb18c1b201ac8e953fd7b
Micah Stotsky <Leowyatt09@gmail.com>
2023-01-26 22:50:08 -0600

    get race done

--- dragonV2/urls.py
+++ dragonV2/urls.py
@@ -1,21 +1,17 @@
-"""dragonV2 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from dragonapi.views import register_user, check_user
+from django.conf.urls import include
+from rest_framework import routers
+from dragonapi.views import RaceView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'race', RaceView, 'race')
+
 
 urlpatterns = [
+    path('register', register_user),
+    path('checkuser', check_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
