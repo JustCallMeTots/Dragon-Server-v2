240fec4d59d27c5d5694191eb8492b30e48e4503
Micah Stotsky <Leowyatt09@gmail.com>
2023-01-30 17:50:52 -0600

    closer

--- dragonapi/models/character.py
+++ dragonapi/models/character.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 from .race import race
-from .classes import classes
 from .user import user
 from .equipment import equipment
 
@@ -10,11 +9,12 @@ class character(models.Model):
   name = models.CharField(max_length=55)
   level = models.CharField(max_length=55)
   race = models.ForeignKey(race, on_delete=models.CASCADE)
-  classes_name = models.ForeignKey(classes, on_delete=models.CASCADE)
   ability = models.CharField(max_length=55)
   description = models.CharField(max_length=55)
   equipment = models.ForeignKey(equipment, on_delete=models.CASCADE)
   spells = models.CharField(max_length=55)
   alive = models.BooleanField(default=True)
-  
-#   possible foreign key for race and class?
+  @property
+  def charclasses(self):
+      charclasses = [classes for classes in self.character_classes.all()]
+      return charclasses
